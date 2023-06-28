5dc37145ec18ca1af07cd52f5b7b94742067563e
lyq12345 <yuqiaol5@uci.edu>
2023-06-28 16:16:59 -0700

    xxx

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,9 @@
 /**
  * Created by hao.cheng on 2017/5/3.
  */
-import React, {useState, useEffect} from 'react';
-import ReactJson from 'react-json-view'
-import { Row, Col, Card, Timeline, Button, Modal, List  } from 'antd';
+import React, { useState, useEffect } from 'react';
+import ReactJson from 'react-json-view';
+import { Row, Col, Card, Timeline, Button, Modal, List } from 'antd';
 import BreadcrumbCustom from '../widget/BreadcrumbCustom';
 import EchartsViews from './EchartsViews';
 import EchartsProjects from './EchartsProjects';
@@ -21,108 +21,106 @@ import {
 } from '@ant-design/icons';
 
 const Dashboard = () => {
-    const [plan, setPlan] = useState([])
-    const [graph, setGraph] = useState({})
-    const [selectedOps, setSelectedOps] = useState([])
-    const [result, setResult] = useState([])
+    const [plan, setPlan] = useState([]);
+    const [graph, setGraph] = useState({});
+    const [selectedOps, setSelectedOps] = useState([]);
+    const [result, setResult] = useState([]);
     const onDeployButtonClicked = () => {
         console.log(selectedOps);
         fetchDeploymentPlan(selectedOps).then((data) => {
             setPlan(data);
-        })
-        showModal()
-    }
+        });
+        showModal();
+    };
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const getResult = ()=> {
+    const getResult = () => {
         fetchResults().then((data) => {
-            setResult(data)
+            setResult(data);
         });
-    }
+    };
 
     useEffect(() => {
-        getResult()
-        getAndstartDeployment({}).then(res => {
+        getResult();
+        getAndstartDeployment({}).then((res) => {
             setGraph(res);
-        })
+        });
     }, []);
 
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
 
-  const handleOk = () => {
-    let params = {
-        data: plan
-    }
-    getAndstartDeployment(params).then(res => {
-        // loading
-        setGraph(res);
+    const handleOk = () => {
+        let params = {
+            data: plan,
+        };
+        getAndstartDeployment(params).then((res) => {
+            // loading
+            setGraph(res);
+            setIsModalOpen(false);
+        });
+    };
+
+    const handleCancel = () => {
         setIsModalOpen(false);
-    })
-    
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-        return (
-            <div className="gutter-example button-demo">
-                <BreadcrumbCustom />
-                <Row gutter={12}>
-                    <Col className="gutter-row" md={12}>
-                        <Card bordered={false}>
-                        <div className='gutter-box' style={{display:"flex"}}>
+    };
+    return (
+        <div className="gutter-example button-demo">
+            <BreadcrumbCustom />
+            <Row gutter={12}>
+                <Col className="gutter-row" md={16}>
+                    <Card bordered={false}>
+                        <div className="gutter-box" style={{ display: 'flex' }}>
                             <DeviceLib />
-                            <OperatorLib setSelectedOps={setSelectedOps}/>
+                            <OperatorLib setSelectedOps={setSelectedOps} />
                         </div>
                         <Button onClick={onDeployButtonClicked}>Create Deployment Plan</Button>
                         <Modal onOk={handleOk} onCancel={handleCancel} visible={isModalOpen}>
                             <p>Deployment plan generated. Confirm to proceed.</p>
                             <ReactJson src={plan} />
                         </Modal>
+                    </Card>
+                </Col>
+                <Col className="gutter-row" md={8}>
+                    <div className="gutter-box">
+                        <Card bordered={false} className={'no-padding'}>
+                            {/* <EchartsProjects /> */}
+                            <EchartsForce graph={graph} />
                         </Card>
-                    </Col>
-                    <Col className="gutter-row" md={12}>
-                        <div className="gutter-box">
-                            <Card bordered={false} className={'no-padding'}>
-                                {/* <EchartsProjects /> */}
-                                <EchartsForce graph={graph}/>
-                            </Card>
-                        </div>
-                    </Col>
-                </Row>
-                <Row gutter={12}>
-                    <Col className="gutter-row" md={24}>
-                        <div className='gutter-box'>
-                            <Card bordered={false}>
-                                <div className="pb-m">
-                                    <h3>Results</h3>
-                                </div>
-                                <span className="card-tool">
-                                    <SyncOutlined onClick={getResult}/>
-                                </span>
-                                <List
-                                    itemLayout="horizontal"
-                                    dataSource={result}
-                                    renderItem={item => (
-                                    <List.Item style={{display:"block"}}>
-                                        <List.Item.Meta
-                                        title={<a href="https://ant.design">{item['link']}</a>}
-                                        />
-                                     
-                                            <ReactJson src={item['value']} />
- 
-                                    </List.Item>
-                                    )}
-                                />
-                            </Card>
+                    </div>
+                </Col>
+            </Row>
+            <Row gutter={12}>
+                <Col className="gutter-row" md={24}>
+                    <div className="gutter-box">
+                        <Card bordered={false}>
+                            <div className="pb-m">
+                                <h3>Results</h3>
                             </div>
-                    </Col>
-                </Row>
-            </div>
-        );
-    }
+                            <span className="card-tool">
+                                <SyncOutlined onClick={getResult} />
+                            </span>
+                            <List
+                                itemLayout="horizontal"
+                                dataSource={result}
+                                renderItem={(item) => (
+                                    <List.Item style={{ display: 'block' }}>
+                                        <List.Item.Meta
+                                            title={<a href="https://ant.design">{item['link']}</a>}
+                                        />
+
+                                        <ReactJson src={item['value']} />
+                                    </List.Item>
+                                )}
+                            />
+                        </Card>
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    );
+};
 
 export default Dashboard;
--- src/components/dashboard/DeviceLib.jsx
+++ src/components/dashboard/DeviceLib.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchDevices } from '../../service';
-import {List, Card} from "antd"
-
+import { List, Card } from 'antd';
 
 const DeviceLib = () => {
     const [data, setData] = useState([]);
@@ -17,19 +16,20 @@ const DeviceLib = () => {
     }, []);
 
     return (
-        <div style={{border:"1px solid #000", margin: "0 2px"}}>
-          <h3>Devices</h3>
-    <List className='gutter-list'
-            grid={{ gutter: 8, column: 4 }}
-            dataSource={data}
-            renderItem={item => (
-      <List.Item>
-        <Card title={item.name}>{item.type}</Card>
-      </List.Item>
-    )}
-  />
+        <div style={{ border: '1px solid #000', margin: '0 2px' }}>
+            <h3>Devices</h3>
+            <List
+                className="gutter-list"
+                grid={{ gutter: 8, column: 4 }}
+                dataSource={data}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Card title={item.name}>{item.type}</Card>
+                    </List.Item>
+                )}
+            />
         </div>
-    )
-}
+    );
+};
 
-export default DeviceLib
+export default DeviceLib;
